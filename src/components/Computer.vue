a4bbd0edba731ef06194c8f89e203bfa60b9caf1
Jurgo <jurgo88@gmail.com>
2024-01-29 23:59:28 +0100

    Computer update

--- src/components/Computer.vue
+++ src/components/Computer.vue
@@ -9,46 +9,7 @@
         <p><strong>IP Address:</strong> {{ ipAddress }}</p>
         <p><strong>Username:</strong> {{ username }}</p>
     </div>
-    <div style="height: auto;">
-        <h3>My Computer</h3>
-        <p><strong>Operating System:</strong> {{ operatingSystem }}</p>
-        <p><strong>Platform:</strong> {{ platform }}</p>
-        <p><strong>Engine:</strong> {{ engine }}</p>
-        <p><strong>Browser:</strong> {{ browser }}</p>
-        <p><strong>Screen Resolution:</strong> {{ screenResolution }}</p>
-        <p><strong>IP Address:</strong> {{ ipAddress }}</p>
-        <p><strong>Username:</strong> {{ username }}</p>
-    </div>
-    <div style="height: auto;">
-        <h3>My Computer</h3>
-        <p><strong>Operating System:</strong> {{ operatingSystem }}</p>
-        <p><strong>Platform:</strong> {{ platform }}</p>
-        <p><strong>Engine:</strong> {{ engine }}</p>
-        <p><strong>Browser:</strong> {{ browser }}</p>
-        <p><strong>Screen Resolution:</strong> {{ screenResolution }}</p>
-        <p><strong>IP Address:</strong> {{ ipAddress }}</p>
-        <p><strong>Username:</strong> {{ username }}</p>
-    </div>
-    <div style="height: auto;">
-        <h3>My Computer</h3>
-        <p><strong>Operating System:</strong> {{ operatingSystem }}</p>
-        <p><strong>Platform:</strong> {{ platform }}</p>
-        <p><strong>Engine:</strong> {{ engine }}</p>
-        <p><strong>Browser:</strong> {{ browser }}</p>
-        <p><strong>Screen Resolution:</strong> {{ screenResolution }}</p>
-        <p><strong>IP Address:</strong> {{ ipAddress }}</p>
-        <p><strong>Username:</strong> {{ username }}</p>
-    </div>
-    <div style="height: auto;">
-        <h3>My Computer</h3>
-        <p><strong>Operating System:</strong> {{ operatingSystem }}</p>
-        <p><strong>Platform:</strong> {{ platform }}</p>
-        <p><strong>Engine:</strong> {{ engine }}</p>
-        <p><strong>Browser:</strong> {{ browser }}</p>
-        <p><strong>Screen Resolution:</strong> {{ screenResolution }}</p>
-        <p><strong>IP Address:</strong> {{ ipAddress }}</p>
-        <p><strong>Username:</strong> {{ username }}</p>
-    </div>
+
 </template>
 
 <script setup>
