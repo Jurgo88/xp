5e86b6cef5408a1246e1a82e6952d5794354d5e8
Jurgo <jurgo88@gmail.com>
2024-01-31 02:37:44 +0100

    Start Taskbar updated

--- src/components/WinBar.vue
+++ src/components/WinBar.vue
@@ -1,32 +1,28 @@
 <template>
     <div class="taskbar">
         <div class="start-button">
-            <img src="https://icons.iconarchive.com/icons/kyo-tux/aeon/64/System-Windows-icon.png" alt="Start Button" />
+            <img src="../assets/icons/start.png" alt="Start Button" />
         </div>
         <div class="taskbar-icons">
-            <div v-for="icon in taskbarIcons" :key="icon.id" class="taskbar-icon">
-                <img :src="icon.icon" :alt="icon.name" />
-                <span>{{ icon.name }}</span>
-            </div>
+            Tu budu Ikonky SPD
         </div>
         <div class="system-tray">
-            <!-- System tray icons -->
+            Tu budu Mionimalizovane picovine
+        </div>
+        <div class="time">
+            <span>10:00 AM</span>
         </div>
     </div>
 </template>
 
-<script>
-export default {
-    data() {
-        return {
-            taskbarIcons: [
-                { id: 1, name: "My Computer", icon: "https://icons.iconarchive.com/icons/icons-land/vista-hardware-devices/48/Computer-icon.png" },
-                { id: 2, name: "Internet Explorer", icon: "/path/to/internet-explorer-icon.png" },
-                // Add more icons as needed
-            ]
-        };
-    }
-};
+<script setup>
+const taskbarIcons = [
+  { id: 1, name: "My Computer", icon: "https://icons.iconarchive.com/icons/icons-land/vista-hardware-devices/48/Computer-icon.png" },
+  { id: 2, name: "Internet Explorer", icon: "/path/to/internet-explorer-icon.png" },
+    { id: 3, name: "Recycle Bin", icon: "/path/to/recycle-bin-icon.png" },
+  // Add more icons as needed
+
+];
 </script>
 
 <style scoped>
@@ -35,12 +31,11 @@ export default {
     bottom: 0;
     left: 0;
     width: 100%;
-    height: 70px;
+    height: 40px;
     background-color: #1e69de;
     display: flex;
     align-items: center;
     justify-content: space-between;
-    padding: 0 10px;
 }
 
 .start-button {
@@ -54,6 +49,9 @@ export default {
     display: flex;
     align-items: center;
     gap: 10px;
+    flex: 1;
+    border-right: 4px solid #0e51bd;
+    height: 80%;
 }
 
 .taskbar-icon {
@@ -61,7 +59,7 @@ export default {
     flex-direction: column;
     align-items: center;
     gap: 5px;
-    color: #fff;
+    color: #da1e1e;
 }
 
 .system-tray {
@@ -69,5 +67,18 @@ export default {
     align-items: center;
     gap: 10px;
     color: #fff;
+    flex: 8;
+}
+.time {
+    display: flex;
+    align-items: center;
+    gap: 5px;
+    color: #fff;
+    flex: 1;
+}
+.time span {
+    font-size: 12px;
+    position: absolute;
+    right: 20px;
 }
 </style>
