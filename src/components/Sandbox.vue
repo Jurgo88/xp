571d861c6b8258d18ffce04c37ef2e51fca72780
Jurgo <jurgo88@gmail.com>
2024-02-12 23:43:24 +0100

    Images for card

--- src/components/Sandbox.vue
+++ src/components/Sandbox.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="sandbox">
+        <div class="imageTop">Kokot</div>
         <div class="titleWrap">
             <h2 class="myTitle" :style="myTitleStyle">{{ titleFirstLine }}
                 <div class="titleBackground" :style="myTitleBackgroundStyle"></div>
@@ -16,7 +17,8 @@
         <br>
         <br>
         <br>
-        
+        <div class="imageBottom">Kokot</div>
+
         <CustomButton buttonText="Learn More" height="" width="" @click="handleClick"/>
     </div>
 </template>
