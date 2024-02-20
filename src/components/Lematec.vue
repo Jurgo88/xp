39d8ff985ac14ab09df2f8ac1e64ceab0ecdae84
Jurgo <jurgo88@gmail.com>
2024-02-20 16:52:02 +0100

    Updated Lematec

--- src/components/Lematec.vue
+++ src/components/Lematec.vue
@@ -7,106 +7,77 @@
       <input type="number" v-model="ramp" @input="updateCurrent" min="0" />
   
       <p v-if="!isLoading">Výstup: {{ outCurrent.toFixed(2) }} mA</p>
+      <p v-if="isLoading" class="counting">
+        Loading...<br>
+        rampTime: {{ rampTime.toFixed(2) }}s
+        </p>
     </div>
   </template>
   
   <script setup>
-  import { ref, watch } from 'vue';
-  
-  const command = ref(0);
-  const ramp = ref(3);
-  const outCurrent = ref(4);
-  const oldCommand = ref(0);
-  const rampTime = ref(0);
+    import { ref, watch } from 'vue';
+    
+    const command = ref(0);
+    const ramp = ref(10);
+    const outCurrent = ref(4);
+    const oldCommand = ref(0);
+    const rampTime = ref(0);
     const isLoading = ref(false);
   
-  const updateCurrent = () => {
+    const updateCurrent = () => {
+        isLoading.value = true;
+        if (command.value > 100) {
+        command.value = 100;
+        }
+        if (command.value < 0) {
+        command.value = 0;
+        }
 
-    if (command.value > 100) {
-      command.value = 100;
-    }
-    if (command.value < 0) {
-      command.value = 0;
-    }
-
-    const targetCurrent = 4 + (20 - 4) * (command.value / 100);
-    // const rampTime = (ramp.value ) * (command.value / 100);
-
-    // console.log('command', command.value);
-
-    // console.log('ramnpTime', rampTime + 's');
+        const targetCurrent = 4 + (20 - 4) * (command.value / 100);
+        // const rampTime = (ramp.value ) * (command.value / 100);
 
 
-    // const myInterval = setInterval(() => {
-    //   outCurrent.value = targetCurrent;
-    //   console.log('outCurrent', outCurrent.value);
-    // }, rampTime * 1000);
 
-    // setTimeout(() => {
-    //   outCurrent.value = targetCurrent;
-    // //   console.log('outCurrent', outCurrent.value);
-    // }, rampTime * 1000);
+        console.log('targetCurrent', targetCurrent);
+
+        outCurrent.value = targetCurrent;
+
+    };
+
+    const dropRanger = () => {
+        console.log('drop the ranger');
+        console.log('oldCommand', oldCommand.value);
+        console.log('command', command.value);
+        calculateRampTime();
+        updateCurrent();
+    };
+
+    const setOldVariabiles = () => {
+        console.log('set old variabiles');
+        oldCommand.value = command.value;
 
 
-    // // Výpočet kroku změny proudu
-    // for(let i = 0; i <= rampTime*1000; i++) {
-    //     // console.log('i', i);
-    //     // outCurrent.value =targetCurrent;
-       
-            
-    // }
+    };
+
+    const calculateRampTime = () => {
+        // console.log('calculateRampTime');
+        let difference = Math.abs(command.value - oldCommand.value);
+        console.log('rozdiel', difference + '%');
+        rampTime.value= (ramp.value ) * (difference / 100);
+        console.log('rampTime', rampTime.value + 's');
+        setTimeout(() => {
+            isLoading.value = false;
+        }, rampTime.value * 1000);
+        for(let i = 0; i < rampTime.value * 10; i++) {
+            console.log('i', i);
+            // this.$el.counting.innerHTML = 'Loading...<br>';
+        }
+
+    };
     
-
-    // for (let i = 0; i <= command.value; i++) {
-    // //   console.log('outCurrent', i);
-    //   setTimeout(() => {
-    //     console.log('myI', i);
-    //     outCurrent.value = 4 + (20 - 4) * (i / 100);
-    //   }, ramnpTime * 1000);
-      
-    // }
-
-
-    console.log('targetCurrent', targetCurrent);
-
-    // Výpočet kroku změny proudu
-
-    outCurrent.value = targetCurrent;
-
-    // Rampa - postupné zvyšování/snižování proudu
-    // for (let i = 0; i < ramp.value * 10; i++) {
-    //     outCurrent.value += stepChange;
-    //     // console.log('outCurrent', outCurrent.value);
-    // }
-  };
-
-const dropRanger = () => {
-console.log('drop the ranger');
-console.log('oldCommand', oldCommand.value);
-console.log('command', command.value);
-calculateRampTime();
-updateCurrent();
-};
-
-const setOldVariabiles = () => {
-    console.log('set old variabiles');
-    oldCommand.value = command.value;
-
-
-};
-
-const calculateRampTime = () => {
-    console.log('calculateRampTime');
-    let difference = Math.abs(command.value - oldCommand.value);
-    console.log('rozdiel', difference + '%');
-    rampTime .value= (ramp.value ) * (difference / 100);
-    console.log('rampTime', rampTime.value + 's');
-
-};
-  
-watch([command, ramp], () => {
-// updateCurrent();
-});
+    watch([command, ramp], () => {
+    // updateCurrent();
+    });
 </script>
 ```
 <style>
