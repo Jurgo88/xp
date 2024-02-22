c40e561d2068841d42b3784b451f9790adea8f2a
Jurgo <jurgo88@gmail.com>
2024-02-22 23:58:41 +0100

    Updated Lematec

--- src/components/Lematec.vue
+++ src/components/Lematec.vue
@@ -1,29 +1,29 @@
 <template>
     <div>
-      <label for="command">Command {{ command }} [%]:</label>
-      <input id="ranger" type="range" v-model="command" @input="updateCurrent" @mousedown="setOldVariabiles" @mouseup="dropRanger()" min="0" max="100" />
-  
-      <label for="ramp">Čas změny [s]:</label>
-      <input type="number" v-model="ramp" @input="updateCurrent" min="0" />
-  
-      <p v-if="!isLoading">Výstup: {{ outCurrent.toFixed(2) }} mA</p>
-      <p v-if="isLoading" class="counting">
+        <label for="command">Command {{ command }} [%]:</label>
+        <input id="ranger" type="range" v-model="command" @input="updateCurrent" @mousedown="setOldVariabiles" @mouseup="dropRanger()" min="0" max="100" />
+
+        <label for="ramp">Čas změny [s]:</label>
+        <input type="number" v-model="ramp" @input="updateCurrent" min="0" />
+
+        <p v-if="!isLoading">Výstup: {{ outCurrent.toFixed(2) }} mA</p>
+        <p v-if="isLoading" class="counting">
         Loading...<br>
         rampTime: {{ rampTime.toFixed(2) }}s
         </p>
     </div>
-  </template>
-  
-  <script setup>
+</template>
+
+<script setup>
     import { ref, watch } from 'vue';
-    
+
     const command = ref(0);
     const ramp = ref(10);
     const outCurrent = ref(4);
     const oldCommand = ref(0);
     const rampTime = ref(0);
     const isLoading = ref(false);
-  
+
     const updateCurrent = () => {
         isLoading.value = true;
         if (command.value > 100) {
@@ -75,22 +75,22 @@
         }
 
     };
-    
+
     watch([command, ramp], () => {
     // updateCurrent();
     });
 </script>
-```
+
 <style>
-input {
-  width: 95%;
-  padding: 12px 20px;
-  margin: 8px 0;
-  box-sizing: border-box;
-}
-label {
-  display: block;
-  margin-top: 10px;
-}
+    input {
+    width: 95%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    box-sizing: border-box;
+    }
+    label {
+    display: block;
+    margin-top: 10px;
+    }
 </style>
   
